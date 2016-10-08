5ba74001f5e96ec224d04bcaeecd67bea98aad8a
Lexdysic <lexdysic@gmail.com>
2016-10-07 20:54:55 -0700

    Updates
    Detects motion in the trigger window and computes current power usage,

--- PowerWatch.py
+++ PowerWatch.py
@@ -1,18 +1,43 @@
 import cv2
 import numpy as np
+import time
 
+# State
+kStateInit = 0
+kStateOut  = 1
+kStateIn   = 2
 
-RED   = (0, 0, 255)
-GREEN = (0, 255, 0)
-BLUE  = (255, 0, 0)
+# Key
+kKeyDown   = 2621440
+kKeyUp     = 2490368
+kKeyRight  = 2555904
+kKeyLeft   = 2424832
+kKeySpace  = 32
+kKeyEscape = 27
+
+# Time
+kSecondsPerMinute = 60
+kMinutesPerHour = 60
+kSecondsPerHour = kSecondsPerMinute * kMinutesPerHour
+
+# Color
+kRed   = (0, 0, 255)
+kGreen = (0, 255, 0)
+kBlue  = (255, 0, 0)
+
+# Power Meter
+kWattHoursPerRev = 7.2
+
+# Config
+kMinInsideTime = 1.0
 
 def lerp(a, b, t):
     return cv2.addWeighted(a, 1-t, b, t, 0)
 
 def overlap(aTL, aBR, bTL, bBR):
-    if aTL[0] < bBR[0]:
+    if aTL[0] > bBR[0]:
         return False
-    if aTL[1] < bBR[1]:
+    if aTL[1] > bBR[1]:
         return False
     if aBR[0] < bTL[0]:
         return False
@@ -24,57 +49,103 @@ def runPowerWatch():
     cv2.namedWindow("preview")
     cap = cv2.VideoCapture(0)
 
+    state = kStateInit
+    lastInsideTime = time.clock()
+    lastKnownPower = None
+    lastTriggerTime = None
     lastGray = None
+    displayIndex = 0
     while cap.isOpened():
         success, frameBgr = cap.read()
-        if success:
-            currGray = cv2.cvtColor(frameBgr, cv2.COLOR_BGR2GRAY)
-            currGray = cv2.GaussianBlur(currGray, (21, 21), 0)
-            if lastGray is None:
-                lastGray = currGray
+        if not success:
+            break
 
-            deltaGray = cv2.absdiff(lastGray, currGray)
-            thresh = cv2.threshold(deltaGray, 25, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+        # Process the frame to obtain contours
+        currGray = cv2.cvtColor(frameBgr, cv2.COLOR_BGR2GRAY)
+        currGray = cv2.GaussianBlur(currGray, (21, 21), 0)
+        if lastGray is None:
             lastGray = currGray
 
-            display = frameBgr
+        deltaGray    = cv2.absdiff(lastGray, currGray)
+        threshGray   = cv2.threshold(deltaGray, 30, 255, cv2.THRESH_BINARY)[1]
+        dialatedGray = cv2.dilate(threshGray, None, iterations=2)
 
-            # compute trigger window
-            screenH, screenW, _ = display.shape
-            triggerX0, triggerY0 = screenW // 4, screenH * 2 // 4
-            triggerX1, triggerY1 = triggerX0 + screenW // 2, triggerY0 + screenH // 4
-
-            triggerBL = (triggerY0, triggerX0)
-            triggerTR = (triggerY1, triggerX1)
+        contours, _ = cv2.findContours(dialatedGray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        lastGray = currGray
 
 
-            triggered = False
+        # Set up the image that will end up being displayed to screen
+        images      = [frameBgr, currGray, deltaGray, threshGray, dialatedGray]
+        imageCount  = len(images)
+        display     = images[displayIndex]
+        showOverlay = display is frameBgr
 
-            # loop over the contours
-            for c in cnts:
-                if cv2.contourArea(c) < 500:
-                    continue
-                (contourX0, contourY0, contourW, contourH) = cv2.boundingRect(c)
-                contourX1, contourY1 = contourX0 + contourW, contourY0 + contourH
-                contourBL = (contourY0, contourX0)
-                contourTR = (contourY1, contourX1)
-                cv2.rectangle(display, (contourX0, contourY0), (contourX1, contourY1), GREEN, 2)
+        # compute trigger window
+        screenH, screenW     = display.shape[0], display.shape[1]
+        triggerW, triggerH   = screenW  * 1 // 3, screenH * 1 // 4
+        triggerX0, triggerY0 = screenW * 1 // 3, screenH * 2 // 4
+        triggerX1, triggerY1 = triggerX0 + triggerW, triggerY0 + triggerH
+        triggerTL            = (triggerY0, triggerX0)
+        triggerBR            = (triggerY1, triggerX1)
 
-                if not triggered and overlap(triggerTR, triggerBL, contourTR, contourBL):
-                    triggered = True
 
-            cv2.rectangle(display, (triggerX0, triggerY0), (triggerX1, triggerY1), RED if triggered else BLUE, 2)
+        # Determine if any of the contours overlap the trigger window
+        triggered = False
+        for contour in contours:
+            if cv2.contourArea(contour) < 500:
+                continue
+            contourX0, contourY0, contourW, contourH = cv2.boundingRect(contour)
+            contourX1, contourY1 = contourX0 + contourW, contourY0 + contourH
+            contourTL = (contourY0, contourX0)
+            contourBR = (contourY1, contourX1)
 
-            cv2.imshow("preview", display)
+            if showOverlay:
+                cv2.rectangle(display, (contourX0, contourY0), (contourX1, contourY1), kGreen, 2)
 
-            key = cv2.waitKey(20)
-            if key == 27: # exit on ESC
-                break
-        else:
+            if not triggered and overlap(triggerTL, triggerBR, contourTL, contourBR):
+                triggered = True
+        if showOverlay:
+            cv2.rectangle(display, (triggerX0, triggerY0), (triggerX1, triggerY1), kRed if triggered else kBlue, 2)
+
+
+        # Process the state machine
+        currTime = time.clock()
+        if state == kStateInit:
+            if triggered:
+                state = kStateIn
+        elif state == kStateIn:
+            if not triggered and (currTime - lastInsideTime) > kMinInsideTime:
+                state = kStateOut
+        elif state == kStateOut:
+            if triggered:
+                state = kStateIn
+                lastInsideTime = currTime
+
+                currTriggerTime = currTime
+                if lastTriggerTime:
+                    secondsPerRev = currTriggerTime - lastTriggerTime
+                    lastKnownPower = kWattHoursPerRev * kSecondsPerHour / secondsPerRev
+                lastTriggerTime = currTriggerTime
+
+
+        # Print the current state
+        if showOverlay:
+            stateValue = "Init" if state == kStateInit else "In" if state == kStateIn else "Out"
+            text = "State: {} | Power Usage: {} Watts".format(stateValue, lastKnownPower)
+            cv2.putText(display, text, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, kRed, 2)
+
+        # Display the image
+        cv2.imshow("preview", display)
+
+        # Process key press
+        key = cv2.waitKey(20)
+        if key == kKeyEscape:
             break
+        elif key == kKeyUp:
+            displayIndex = (displayIndex + imageCount - 1) % imageCount
+        elif key == kKeyDown:
+            displayIndex = (displayIndex + 1) % imageCount
+        
     cap.release()
     cv2.destroyWindow("preview")
 
